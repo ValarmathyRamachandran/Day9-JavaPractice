472ed9834dfa97661d638b976806562ef0d56108
Valarmathy <valarmathy.ramachandran@outlook.com>
2021-10-08 00:33:33 +0530

    Calculated Employee Wages for Multiple Companies

---
+++ EmpWageBuilderUC7.java
@@ -0,0 +1,44 @@
+package javapractice;
+
+public class EmpWageBuilderUC6 {
+	public static final int IS_FULL_TIME = 1;
+    	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+    	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
+	
+
+private static int getTotalEmpWage(){
+	 System.out.println( "Welcome to Employee Wage Computation Program");
+     int empHrs =0, totalEmpHrs=0, totalWorkingDays =0;
+
+
+     while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+     totalWorkingDays++;
+     int empCheck = (int)Math.round((Math.random() *10) % 3);
+     switch (empCheck)
+     {
+				case IS_FULL_TIME:
+						empHrs=8;
+						break;
+				case IS_PART_TIME:
+						empHrs=4;
+						break;
+				default:
+						empHrs=0;
+		}
+		totalEmpHrs += empHrs;
+		System.out.println("day:" + totalWorkingDays +" " + "Emp hr: " +empHrs);
+	}
+	int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+	System.out.println("Total Emp Wage: " + totalEmpWage);
+	return totalEmpWage;
+}
+
+        public static void main(String[] args) {
+
+        	
+        	getTotalEmpWage();
+               
+		}
+}
